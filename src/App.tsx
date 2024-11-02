4fc8676953337f7d9dd05e358009e8cb6a9bb02c
Arthur Filipe <165584196+ArthurFQT@users.noreply.github.com>
2024-11-01 22:22:37 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GlobalStyles from './styles/globalStyles';
+
+function App() {
+  return (
+    <center>
+      <h1>ARTHUR F. </h1>
+    </center>
+  );
+}
+
+export default App;
